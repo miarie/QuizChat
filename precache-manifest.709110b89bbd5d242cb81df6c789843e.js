c0c6855d5c65fb3e684bb45ac73dec37020348a1
lorenzovon <lazyprogrammerx@gmail.com>
2020-10-09 17:15:20 +0530

    Updates

---
+++ precache-manifest.709110b89bbd5d242cb81df6c789843e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ac47f7248601e4d4185fb35453575f",
+    "url": "/QuizChat/index.html"
+  },
+  {
+    "revision": "c738cc326656a4023d12",
+    "url": "/QuizChat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb7fb9ccf45160a690fe",
+    "url": "/QuizChat/static/js/2.fbf4146e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizChat/static/js/2.fbf4146e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c738cc326656a4023d12",
+    "url": "/QuizChat/static/js/main.c29c8b71.chunk.js"
+  },
+  {
+    "revision": "8b04526d46278856ac18",
+    "url": "/QuizChat/static/js/runtime-main.3bfbd655.js"
+  }
+]);
